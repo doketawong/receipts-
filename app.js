781e7bbb16f8917ce3ca529e64a8aea9ee813d9f
Jason Wong <1155086874@link.cuhk.edu.hk>
2019-08-10 17:06:01 +0800

    successful build

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const express = require("express")
-let app = new express()
+const express = require('express')
+const app = express()
+const port = 8080
 
-let port =8080
-app.listen(8080, function(){
-    console.log("Server started listen at localhost: "+port)
-})
+app.listen(8080, function () {
+  console.log('Server started listen at localhost: ' + port)
+}
+)
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/model/index.html')
+}
+)
